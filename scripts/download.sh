608f63a1688e0b13926410896c5555a6ed454cf8
MarcRubFer <rubio.fernandez.marcos@gmail.com>
2022-11-13 00:47:29 +0100

    Write of download.sh script for $1 to $4 arguments.

--- scripts/download.sh
+++ scripts/download.sh
@@ -1,3 +1,5 @@
+# INSTRUCTIONS--------------------------------------------------------------------------------
+#
 # This script should download the file specified in the first argument ($1),
 # place it in the directory specified in the second argument ($2),
 # and *optionally*:
@@ -16,3 +18,49 @@
 #   CCAGGATTTACAGACTTTAAA
 #
 #   If $4 == "another" only the **first two sequence** should be output
+#-----------------------------------------------------------------------------------------------
+
+# Creation of directory of destiny (if no exists)
+
+mkdir -p $2
+
+# Script for download from $1 and store in $2
+
+file_name=$(basename $1)
+
+echo "Downloading sequence..."
+echo
+
+wget -O $2/$file_name $1
+
+echo "Done"
+echo "----"
+
+# Script for optional uncompress ($3 == yes)
+
+if [ $3 == "yes" ]
+then
+        echo "Uncompressing file..."
+        echo
+
+        gunzip $2/$file_name
+
+        echo "Done"
+        echo "----"
+fi
+
+# Script for filtering by $4
+## NOTE: A new directory ($2/filtered) is created for store filtered sequences; so original sequences are not overwritten.
+
+if [ $# == 4 ]
+then
+        echo "Filtering sequence..."
+        echo
+
+        filename_uncompress=$(basename $file_name .gz)
+        mkdir -p $2/filtered
+        sed -e "/$4/,+3{d}"  $2/$filename_uncompress > $2/filtered/$filename_uncompress-filter_by_$4
+
+        echo "Done"
+        echo "----"
+fi
