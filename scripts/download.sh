694f6251e3665be759107a1822bef1042e001956
MarcRubFer <rubio.fernandez.marcos@gmail.com>
2022-12-16 00:11:58 +0100

    New lines for $4. Reformat script.

--- scripts/download.sh
+++ scripts/download.sh
@@ -1,49 +1,27 @@
-# INSTRUCTIONS--------------------------------------------------------------------------------
-#
-# This script should download the file specified in the first argument ($1),
-# place it in the directory specified in the second argument ($2),
-# and *optionally*:
-# - uncompress the downloaded file with gunzip if the third
-#   argument ($3) contains the word "yes"
-# - filter the sequences based on a word contained in their header lines:
-#   sequences containing the specified word in their header should be **excluded**
-#
-# Example of the desired filtering:
-#
-#   > this is my sequence
-#   CACTATGGGAGGACATTATAC
-#   > this is my second sequence
-#   CACTATGGGAGGGAGAGGAGA
-#   > this is another sequence
-#   CCAGGATTTACAGACTTTAAA
-#
-#   If $4 == "another" only the **first two sequence** should be output
-#-----------------------------------------------------------------------------------------------
-
 # Creation of directory of destiny (if no exists)
 
-mkdir -p $2
+mkdir -p "$2"
 
 # Script for download from $1 and store in $2
 
-file_name=$(basename $1)
+file_name="$(basename "$1")"
 
-echo "Downloading sequence..."
+echo "Downloading sequence(s)..."
 echo
 
-wget -O $2/$file_name $1
+wget -O "$2"/"$file_name" "$1"
 
 echo "Done"
 echo "----"
 
 # Script for optional uncompress ($3 == yes)
 
-if [ $3 == "yes" ]
+if [ "$3" == "yes" ]
 then
         echo "Uncompressing file..."
         echo
 
-        gunzip -k $2/$file_name
+        gunzip -k "$2"/"$file_name"
 
         echo "Done"
         echo "----"
@@ -51,13 +29,16 @@ fi
 
 # Script for filtering by $4
 
-if [ $# == 4 ]
+if [ "$#" == 4 ]
 then
-        echo "Filtering sequence..."
+        echo "Filtering sequence by '"$4"'..."
         echo
 
-        filename_uncompress=$(basename $file_name .gz)
-        seqkit grep -v -r -n -p ".*$4.*" $2/$file_name > $2/$filename_uncompress
+        file_filtered=$(basename "$file_name" .gz)
+	mv "$2"/"$file_filtered" "$2"/temp_"$file_filtered"
+	
+	seqkit grep -v -r -n -p ".*$4.*" $2/temp_"$file_filtered" > "$2"/"$file_filtered"
+	rm "$2"/temp_"$file_filtered"
 
         echo "Done"
         echo "----"
